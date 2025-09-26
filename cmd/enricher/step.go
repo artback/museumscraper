d56e5a478d03490b4cec460a3b569e0497eb7323
Jonathan Artback <jonathan@MacBook-Air.local>
2025-09-26 14:02:15 +0200

    refactor: use models package for MuseumObject in pipeline

--- cmd/enricher/step.go
+++ cmd/enricher/step.go
@@ -1,11 +1,13 @@
 package main
 
+import "museum/models"
+
 type PipelineItem struct {
-	Object  *MuseumObject
+	Object  *models.MuseumObject
 	Results map[string]any
 }
 
-func NewPipelineItem(obj *MuseumObject) *PipelineItem {
+func NewPipelineItem(obj *models.MuseumObject) *PipelineItem {
 	return &PipelineItem{
 		Object:  obj,
 		Results: make(map[string]any),
